3b67bfee877259f6f90b6c1b4b18de7bc2a643f6
Bo Wang <bo@waddle.com.au>
2023-12-02 16:22:48 +1100

    Day 2: Updating format of main class

--- src/day07/Day07.kt
+++ src/day07/Day07.kt
@@ -6,17 +6,17 @@ import readInput
 fun main() {
     val solver = Day07()
     val input = readInput("src/day07/input")
-    println("Part 1: ${solver.part1()}")
-    println("Part 2: ${solver.part2()}")
+    println("Part 1: ${solver.part1(input)}")
+    println("Part 2: ${solver.part2(input)}")
 }
 
 class Day07 {
 
-    fun part1(): Int {
+    fun part1(input: List<String>): Int {
         return 0
     }
 
-    fun part2(): Int {
+    fun part2(input: List<String>): Int {
         return 0
     }
 
