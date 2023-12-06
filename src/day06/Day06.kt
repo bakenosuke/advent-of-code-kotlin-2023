e78c4a9f92c9be745bf0c9c58bc15593ea33e135
Bo Wang <bo@waddle.com.au>
2023-12-06 18:16:28 +1100

    Day 6

--- src/day06/Day06.kt
+++ src/day06/Day06.kt
@@ -12,11 +12,39 @@ fun main() {
 class Day06 {
 
     fun part1(input: List<String>): Int {
-        return 0
+        val time = input[0].substringAfter(":").readNumbers()
+        val distance = input[1].substringAfter(":").readNumbers()
+
+        val solutions = (0..(time.size - 1)).map {
+            getNumberOfSolutions(time[it].toLong(), distance[it].toLong())
+        }
+        println(solutions)
+        var result = 1
+        solutions.forEach {
+            result *= it
+        }
+        return result
+    }
+
+    fun String.readNumbers(): List<Int> {
+        return split(" ").filter { it.isNotEmpty() }.map { it.toInt() }
+    }
+
+    fun getNumberOfSolutions(time: Long, distance: Long): Int {
+        var solutions = 0
+        (1..time).forEach {
+            val timeToFinish = it.toDouble() + (distance.toDouble() / it.toDouble())
+            if(timeToFinish < time) {
+                solutions++
+            }
+        }
+        return solutions
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val time = input[0].substringAfter(":").replace(" ","").toLong()
+        val distance = input[1].substringAfter(":").replace(" ","").toLong()
+        return getNumberOfSolutions(time, distance)
     }
 
 }
--- test/day06/Day06Test.kt
+++ test/day06/Day06Test.kt
@@ -3,6 +3,9 @@ package day06
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 import readInput
+import java.text.SimpleDateFormat
+import java.time.format.DateTimeFormatter
+import java.util.TimeZone
 
 class Day06Test {
 
@@ -11,7 +14,7 @@ class Day06Test {
     @Test
     fun `should match part 1 example` () {
         val input = readInput("test/day06/part1")
-        val expected = 4361
+        val expected = 288
 
         val result = solver.part1(input)
         result shouldBe expected
@@ -20,7 +23,7 @@ class Day06Test {
     @Test
     fun `should match part 2 example` () {
         val input = readInput("test/day06/part1")
-        val expected = 467835
+        val expected = 71503
 
         val result = solver.part2(input)
         result shouldBe expected
