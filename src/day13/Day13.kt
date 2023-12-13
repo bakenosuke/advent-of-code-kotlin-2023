20880738fea6efc9e7dfc07fb4c383a699159eb8
Bo Wang <bo@waddle.com.au>
2023-12-13 22:53:22 +1100

    Day 13

--- src/day13/Day13.kt
+++ src/day13/Day13.kt
@@ -11,12 +11,124 @@ fun main() {
 
 class Day13 {
 
+    data class Reflection(
+        val type: ReflectionType,
+        val index: Int
+    ) {
+        enum class ReflectionType { VERTICAL, HORIZONTAL }
+    }
+
     fun part1(input: List<String>): Long {
-        return 0
+        val groups = input.joinToString("\n").split("\n\n").map {
+            it.split("\n")
+        }
+        val reflections = groups.map { it.findReflection() }
+        val score = reflections.sumOf {
+            when (it.type) {
+                Reflection.ReflectionType.HORIZONTAL -> 100 * it.index
+                Reflection.ReflectionType.VERTICAL -> it.index
+            }
+        }
+
+        return score.toLong()
+    }
+
+    fun List<String>.findReflection(): Reflection {
+        // Vertical
+        (1 until this[0].length).map { i ->
+            val compares = Pair(
+                this.map { it.substring(0, i) },
+                this.map { it.substring(i, it.length) }
+            )
+            if (compares.isVerticalReflection()) {
+                return Reflection(Reflection.ReflectionType.VERTICAL, i)
+            }
+        }
+
+        // Horizontal
+        (1 until this.size).map { i ->
+            val compares = Pair(
+                this.subList(0, i),
+                this.subList(i, this.size)
+            )
+            if (compares.isHorizontalReflection()) {
+                return Reflection(Reflection.ReflectionType.HORIZONTAL, i)
+            }
+        }
+
+        return Reflection(
+            type = Reflection.ReflectionType.VERTICAL,
+            index = 0
+        )
+    }
+
+    fun Pair<List<String>, List<String>>.isVerticalReflection(differences :Int = 0): Boolean {
+        val left = first.map { it.takeLast(second.first().length) }
+        val right = second.map { it.take(first.first().length) }
+
+        return Pair(left, right.map { it.reversed() }).countDifferences() == differences
+    }
+
+    fun Pair<List<String>, List<String>>.isHorizontalReflection(differences :Int = 0): Boolean {
+        val top = first.takeLast(second.size)
+        val bottom = second.take(first.size)
+        return Pair(top, bottom.reversed()).countDifferences() == differences
+    }
+
+    fun Pair<List<String>, List<String>>.countDifferences(): Int {
+        var differences = 0
+        first.forEachIndexed{ rowIndex, row ->
+            row.forEachIndexed { colIndex, col ->
+                if(col != second[rowIndex][colIndex]) {
+                    differences ++
+                }
+            }
+        }
+        return differences
     }
 
     fun part2(input: List<String>): Long {
-        return 0
+        val groups = input.joinToString("\n").split("\n\n").map {
+            it.split("\n")
+        }
+        val reflections = groups.map { it.findReflection2() }
+        val score = reflections.sumOf {
+            when (it.type) {
+                Reflection.ReflectionType.HORIZONTAL -> 100 * it.index
+                Reflection.ReflectionType.VERTICAL -> it.index
+            }
+        }
+
+        return score.toLong()
+    }
+
+    fun List<String>.findReflection2(): Reflection {
+        // Vertical
+        (1 until this[0].length).map { i ->
+            val compares = Pair(
+                this.map { it.substring(0, i) },
+                this.map { it.substring(i, it.length) }
+            )
+            if (compares.isVerticalReflection(1)) {
+                return Reflection(Reflection.ReflectionType.VERTICAL, i)
+            }
+        }
+
+        // Horizontal
+        (1 until this.size).map { i ->
+            val compares = Pair(
+                this.subList(0, i),
+                this.subList(i, this.size)
+            )
+            if (compares.isHorizontalReflection(1)) {
+                return Reflection(Reflection.ReflectionType.HORIZONTAL, i)
+            }
+        }
+
+        return Reflection(
+            type = Reflection.ReflectionType.VERTICAL,
+            index = 0
+        )
     }
 
 }
--- test/day13/Day13Test.kt
+++ test/day13/Day13Test.kt
@@ -11,7 +11,7 @@ class Day13Test {
     @Test
     fun `should match part 1 example`() {
         val input = readInput("test/day13/part1")
-        val expected = 4361
+        val expected = 405
 
         val result = solver.part1(input)
         result shouldBe expected
@@ -20,7 +20,7 @@ class Day13Test {
     @Test
     fun `should match part 2 example`() {
         val input = readInput("test/day13/part1")
-        val expected = 467835
+        val expected = 400
 
         val result = solver.part2(input)
         result shouldBe expected
