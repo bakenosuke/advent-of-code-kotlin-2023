6d3bebf64ac5f422f9f5984217f503adf7b66b91
Bo Wang <bo@waddle.com.au>
2023-12-14 21:24:39 +1100

    Day 14

--- src/day14/Day14.kt
+++ src/day14/Day14.kt
@@ -12,11 +12,174 @@ fun main() {
 class Day14 {
 
     fun part1(input: List<String>): Long {
-        return 0
+        val grid = input.map {
+            it.toCharArray().toMutableList()
+        }.toMutableList()
+
+        grid.print()
+        grid.flipNorth()
+        grid.print()
+
+        return grid.sum()
+    }
+
+    fun MutableList<MutableList<Char>>.print() {
+        println("======")
+        forEachIndexed { rowIndex, row ->
+            println(row.joinToString(""))
+        }
+        println("======")
+    }
+
+    fun MutableList<MutableList<Char>>.sum(): Long {
+        var score = 0L
+        forEachIndexed { rowIndex, row ->
+            row.forEachIndexed { colIndex, col ->
+                if (col == 'O') {
+                    score += this.size - rowIndex
+                }
+            }
+        }
+        return score
+    }
+
+    fun MutableList<MutableList<Char>>.flipNorth(): MutableList<MutableList<Char>> {
+        var found = true
+        while (found) {
+            found = false
+            forEachIndexed { rowIndex, row ->
+                if (rowIndex > 0) {
+                    row.forEachIndexed { colIndex, col ->
+                        val current = this[rowIndex][colIndex]
+                        val above = this[rowIndex - 1][colIndex]
+                        if (current == 'O' && above == '.') {
+                            this[rowIndex][colIndex] = above
+                            this[rowIndex - 1][colIndex] = current
+                            found = true
+                        }
+                    }
+                }
+            }
+        }
+        return this
+    }
+
+    fun MutableList<MutableList<Char>>.flipSouth(): MutableList<MutableList<Char>> {
+        var found = true
+        while (found) {
+            found = false
+            forEachIndexed { rowIndex, row ->
+                if (rowIndex < this.size - 1) {
+                    row.forEachIndexed { colIndex, col ->
+                        val current = this[rowIndex][colIndex]
+                        val below = this[rowIndex + 1][colIndex]
+                        if (current == 'O' && below == '.') {
+                            this[rowIndex][colIndex] = below
+                            this[rowIndex + 1][colIndex] = current
+                            found = true
+                        }
+                    }
+                }
+            }
+        }
+        return this
+    }
+
+    fun MutableList<MutableList<Char>>.flipLeft(): MutableList<MutableList<Char>> {
+        var found = true
+        while (found) {
+            found = false
+            forEachIndexed { rowIndex, row ->
+                row.forEachIndexed { colIndex, col ->
+                    if (colIndex > 0) {
+                        val current = this[rowIndex][colIndex]
+                        val left = this[rowIndex][colIndex - 1]
+                        if (current == 'O' && left == '.') {
+                            this[rowIndex][colIndex] = left
+                            this[rowIndex][colIndex - 1] = current
+                            found = true
+                        }
+                    }
+                }
+            }
+        }
+        return this
+    }
+
+    fun MutableList<MutableList<Char>>.flipRight(): MutableList<MutableList<Char>> {
+        var found = true
+        while (found) {
+            found = false
+            forEachIndexed { rowIndex, row ->
+                row.forEachIndexed { colIndex, col ->
+                    if (colIndex < row.size - 1) {
+                        val current = this[rowIndex][colIndex]
+                        val right = this[rowIndex][colIndex + 1]
+                        if (current == 'O' && right == '.') {
+                            this[rowIndex][colIndex] = right
+                            this[rowIndex][colIndex + 1] = current
+                            found = true
+                        }
+                    }
+                }
+            }
+        }
+        return this
     }
 
     fun part2(input: List<String>): Long {
-        return 0
+        var grid = input.map {
+            it.toCharArray().toMutableList()
+        }.toMutableList()
+
+        grid.print()
+        val patterns = mutableListOf<String>()
+        var consecutiveRepeats = 0L
+        var firstRepeat = -1
+        var repeats = mutableSetOf<Int>()
+        run breaking@{
+            (0..1_000_000_000).forEach {
+                grid.flipNorth()
+                grid.flipLeft()
+                grid.flipSouth()
+                grid.flipRight()
+                if (patterns.contains(grid.toPattern())) {
+                    if (firstRepeat == -1) firstRepeat = it
+                    repeats.add(patterns.indexOf(grid.toPattern()))
+                    consecutiveRepeats++
+                    if (consecutiveRepeats > 1000) {
+                        return@breaking
+                    }
+                } else {
+                    patterns.add(grid.toPattern())
+                    consecutiveRepeats = 0
+                }
+            }
+        }
+
+        println("First $firstRepeat")
+        println("First ${repeats.size}")
+
+        val lookup = 1_000_000_000 - firstRepeat
+        val index = (lookup % repeats.size)
+
+        grid = input.map {
+            it.toCharArray().toMutableList()
+        }.toMutableList()
+
+        repeat(index + firstRepeat) {
+            grid.flipNorth()
+            grid.flipLeft()
+            grid.flipSouth()
+            grid.flipRight()
+        }
+
+        return grid.sum()
     }
 
+    fun MutableList<MutableList<Char>>.toPattern(): String {
+        return joinToString("\n") {
+            it.joinToString("")
+        }
+    }
 }
--- test/day14/Day14Test.kt
+++ test/day14/Day14Test.kt
@@ -11,7 +11,7 @@ class Day14Test {
     @Test
     fun `should match part 1 example`() {
         val input = readInput("test/day14/part1")
-        val expected = 4361
+        val expected = 136
 
         val result = solver.part1(input)
         result shouldBe expected
@@ -20,7 +20,7 @@ class Day14Test {
     @Test
     fun `should match part 2 example`() {
         val input = readInput("test/day14/part1")
-        val expected = 467835
+        val expected = 64
 
         val result = solver.part2(input)
         result shouldBe expected
