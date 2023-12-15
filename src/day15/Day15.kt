9de05b1a2fa63047dc2d6860b2d784bf31c700c4
Bo Wang <bo@waddle.com.au>
2023-12-15 21:43:01 +1100

    Day 15

--- src/day15/Day15.kt
+++ src/day15/Day15.kt
@@ -12,11 +12,68 @@ fun main() {
 class Day15 {
 
     fun part1(input: List<String>): Long {
-        return 0
+        val tokens = input.first().split(",")
+
+        val scores = tokens.map { it.score() }
+
+        return scores.sum()
+    }
+
+    fun String.score(): Long {
+        var i = 0L
+        toCharArray().forEach {
+            i += it.code
+            i *= 17
+            i = i % 256
+        }
+        return i
     }
 
     fun part2(input: List<String>): Long {
-        return 0
+        val tokens = input.first().split(",")
+
+        val boxes = tokens.box()
+
+        var total = 0L
+        boxes.forEachIndexed { boxIndex, box ->
+            box.forEachIndexed { slotIndex, slot ->
+                total += slot.value * (slotIndex + 1) * (boxIndex + 1)
+            }
+        }
+
+        return total
     }
+
+    fun List<String>.box(): List<List<LabelValue>> {
+        val boxes = (0..255).map {
+            mutableListOf<LabelValue>()
+        }
+
+        forEach {
+            val label = it.substringBefore("=").substringBefore("-")
+            val score = label.score()
+            val box = boxes[score.toInt()]
+            when {
+                it.contains('-') -> {
+                    box.removeIf { it.label == label }
+                }
+                it.contains('=') -> {
+                    val value = it.takeLast(1).toLong()
+                    if (box.any { it.label == label }) {
+                        box.first { it.label == label }.value = value
+                    } else {
+                        box.add(LabelValue(label, value))
+                    }
+                }
+            }
+        }
+
+        return boxes
+    }
+
+    data class LabelValue(
+        val label: String,
+        var value: Long,
+    )
 
 }
--- test/day15/Day15Test.kt
+++ test/day15/Day15Test.kt
@@ -11,7 +11,7 @@ class Day15Test {
     @Test
     fun `should match part 1 example`() {
         val input = readInput("test/day15/part1")
-        val expected = 4361
+        val expected = 1320
 
         val result = solver.part1(input)
         result shouldBe expected
@@ -20,7 +20,7 @@ class Day15Test {
     @Test
     fun `should match part 2 example`() {
         val input = readInput("test/day15/part1")
-        val expected = 467835
+        val expected = 145
 
         val result = solver.part2(input)
         result shouldBe expected
